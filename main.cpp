11b6cef8db274eb67a1d9d825d6800252eba5ce8
andreavallebona <115356293+andreavallebona@users.noreply.github.com>
2022-11-27 10:30:23 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,21 @@
 using namespace std;
 
 int main() {
+   int n=0, primonumero=0, secondonumero=1;
+    cin >> n;
+    int c=0;
+    int i=2;
+
+    cout << primonumero+secondonumero << endl;
+
+        while(i++ <=n){
+            c= primonumero+secondonumero;
+            if(c<=n){
+                c= primonumero+secondonumero;
+                cout << c << endl;
+                primonumero= secondonumero;
+                secondonumero= c;
+            }
+        }
    return 0;
 }
